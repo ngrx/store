db100408432bea7f0704add734ac7e1fa3144303
Rob Wormald <robwormald@gmail.com>
2015-12-12 19:58:41 -0800

    chore(tests): initial test setup

---
+++ spec/store_spec.ts
@@ -0,0 +1,9 @@
+import {store} from '../src/store';
+
+describe('sanity check', () => {
+	it('should work', () => {
+		
+		expect(store).toBeDefined()
+		
+	});
+})
---
+++ src/store.ts
@@ -0,0 +1,7 @@
+import {Observable} from 'rxjs';
+
+export const store = () => {
+	return new Observable(val => {
+		
+	});
+}

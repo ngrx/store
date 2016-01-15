4b700e942bf5b125ed75fc751fc45ac7d7fb1a30
Rob Wormald <robwormald@gmail.com>
2016-01-14 17:08:50 -0800

    chore(lint): more cleanup

--- spec/fixtures/counter.ts
+++ spec/fixtures/counter.ts
@@ -3,21 +3,21 @@ export const DECREMENT = 'DECREMENT';
 export const RESET = 'RESET';
 
 export const counterReducer = (state = 0, action) => {
-	switch (action.type) {
-		case INCREMENT:
-			return state + 1;
-			break;
-		
-		case DECREMENT:
-			return state - 1;
-			break;
-			
-		case RESET:
-			return 0;
-			break;
-	
-		default:
-			return state;
-			break;
-	}
-}
+  switch (action.type) {
+    case INCREMENT:
+      return state + 1;
+      break;
+
+    case DECREMENT:
+      return state - 1;
+      break;
+
+    case RESET:
+      return 0;
+      break;
+
+    default:
+      return state;
+      break;
+  }
+};
--- spec/fixtures/todos.ts
+++ spec/fixtures/todos.ts
@@ -1,7 +1,7 @@
 export const ADD_TODO = 'ADD_TODO';
-export const COMPLETE_TODO = 'COMPLETE_TODO'
-export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER'
-export const COMPLETE_ALL_TODOS = 'COMPLETE_ALL_TODOS'
+export const COMPLETE_TODO = 'COMPLETE_TODO';
+export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER';
+export const COMPLETE_ALL_TODOS = 'COMPLETE_ALL_TODOS';
 
 let _id = 0;
 
@@ -9,16 +9,16 @@ export const VisibilityFilters = {
   SHOW_ALL: 'SHOW_ALL',
   SHOW_COMPLETED: 'SHOW_COMPLETED',
   SHOW_ACTIVE: 'SHOW_ACTIVE'
-}
+};
 
 export function visibilityFilter(state = VisibilityFilters.SHOW_ALL, {type, payload}) {
   switch (type) {
     case SET_VISIBILITY_FILTER:
       return payload;
     default:
-      return state
+      return state;
   }
-}
+};
 
 export function todos(state = [], {type, payload}) {
   switch (type) {
@@ -30,14 +30,14 @@ export function todos(state = [], {type, payload}) {
           text: payload.text,
           completed: false
         }
-      ]
+      ];
     case COMPLETE_ALL_TODOS:
-      return state.map(todo => Object.assign({}, todo, {completed: true}))
+      return state.map(todo => Object.assign({}, todo, {completed: true}));
     case COMPLETE_TODO:
       return state.map(todo => {
         return todo.id === payload.id ? Object.assign({}, todo, {completed: true}) : todo;
       });
     default:
-      return state
+      return state;
   }
-}
+};

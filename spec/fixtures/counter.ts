51dcec491e85c55d56f21ef8952b20883af2a3ec
Rob Wormald <robwormald@gmail.com>
2016-02-28 15:33:11 -0800

    fix(specs): remove unreachable code

--- spec/fixtures/counter.ts
+++ spec/fixtures/counter.ts
@@ -6,18 +6,11 @@ export const counterReducer = (state = 0, action) => {
   switch (action.type) {
     case INCREMENT:
       return state + 1;
-      break;
-
     case DECREMENT:
       return state - 1;
-      break;
-
     case RESET:
       return 0;
-      break;
-
     default:
       return state;
-      break;
   }
 };

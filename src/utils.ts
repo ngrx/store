e132d31bb0affaa00c498376e486b223fcb5eda3
Rob Wormald <robwormald@gmail.com>
2016-02-27 18:30:55 -0800

    Revert "feat(store): Moved connection logic to a StoreBackend service"

--- src/utils.ts
+++ src/utils.ts
@@ -1,43 +1,35 @@
 import {Reducer} from './interfaces';
 
-export function combineReducers(reducers: any): Reducer<any> {
+export const combineReducers = (reducers:any): Reducer<any> => {
+
   const reducerKeys = Object.keys(reducers);
-  const finalReducers = {};
 
-  for (let i = 0; i < reducerKeys.length; i++) {
-    const key = reducerKeys[i];
-    if (typeof reducers[key] === 'function') {
-      finalReducers[key] = reducers[key];
-    }
-  }
+  return (state = {}, action) => {
 
-  const finalReducerKeys = Object.keys(finalReducers);
-
-  return function combination(state = {}, action) {
     let hasChanged = false;
     const nextState = {};
-    for (let i = 0; i < finalReducerKeys.length; i++) {
-      const key = finalReducerKeys[i];
-      const reducer = finalReducers[key];
-      const previousStateForKey = state[key];
-      const nextStateForKey = reducer(previousStateForKey, action);
-
-      nextState[key] = nextStateForKey;
-      hasChanged = hasChanged || nextStateForKey !== previousStateForKey;
+    for (var i = 0; i < reducerKeys.length; i++) {
+      const key = reducerKeys[i];
+      const reducer = reducers[key];
+      const previousState = state[key];
+      const newState = reducer(previousState, action);
+      nextState[key] = newState;
+      hasChanged = hasChanged || previousState !== newState;
     }
     return hasChanged ? nextState : state;
-  };
+  }
 }
+
 
 export const compose = (...funcs) => {
   return (...args) => {
     if (funcs.length === 0) {
-      return args[0];
+      return args[0]
     }
 
-    const last = funcs[funcs.length - 1];
-    const rest = funcs.slice(0, -1);
+    const last = funcs[funcs.length - 1]
+    const rest = funcs.slice(0, -1)
 
-    return rest.reduceRight((composed, f) => f(composed), last(...args));
-  };
-};
+    return rest.reduceRight((composed, f) => f(composed), last(...args))
+  }
+}

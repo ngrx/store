eb1d5fef8a88d3ffce99355a9fc667e3619c3d68
Tomas Trajan <tomas.trajan@gmail.com>
2016-05-09 11:43:10 -0700

    chore(tslint): add no-unused-vars rule fixes #115
    - code cleanup
    - run lint as part of test

--- src/dispatcher.ts
+++ src/dispatcher.ts
@@ -1,7 +1,4 @@
 import {Subject} from 'rxjs/Subject';
-import {Observable} from 'rxjs/Observable';
-
-import {Action} from './interfaces';
 
 export class Dispatcher<T> extends Subject<T> {
   dispatch(action: T): void {
